bd671bc63d0fd38073fcac81b388aec0ae7e148f
vishnu <vishnumvn@gmail.com>
2015-08-27 00:09:10 -0700

    Initial Checkin

---
+++ DevOpsQ/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(DevOpsQ.Startup))]
+namespace DevOpsQ
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
